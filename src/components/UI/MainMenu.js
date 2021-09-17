ccf1056dd06ee6b2255be12a55ed96bb5155e468
Uriel Carrillo <carrillouriel@gmail.com>
2021-09-17 16:38:54 -0700

    avoid console errors and unused variables

--- src/components/UI/MainMenu.js
+++ src/components/UI/MainMenu.js
@@ -4,7 +4,6 @@ import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 
 const MainMenu = props =>{
@@ -19,8 +18,8 @@ const onClickHandler = () =>{
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <FavoriteIcon onClick={onClickHandler} />
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={onClickHandler} >
+            <FavoriteIcon />
           </IconButton>
           <Typography variant="h6" color="inherit" component="div">
             {isFavVisible ? <div>Gallery</div> : <div>Favorites</div>}
